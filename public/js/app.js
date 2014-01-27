aa785f2f61afb9ffd7ff24bf3f53f1633fe20f4c
Nathan Ward <ward@digivine.com>
2014-01-26 23:50:54 -0800

    Add simple sampler, triggered by note on/off events

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@
   "use strict";
   var App = {};
   App.load = function () {
+    // MIDI
     if (navigator.requestMIDIAccess) {
       navigator.requestMIDIAccess().then(function (midiAccess) {
         var inputs = midiAccess.inputs();
@@ -18,6 +19,34 @@
     } else {
       window.alert('Your browser does not support MIDI input. Please use Google Chrome Canary.');
     }
+    // Audio
+    App.loadSounds();
+  };
+  App.audioContext = (window.webkitAudioContext) ? new webkitAudioContext() : new AudioContext();
+  App.audioBuffer = null;
+  App.audioSources = {};
+  App.loadSounds = function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'ping.wav', true);
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function(event) {
+      App.audioBuffer = App.audioContext.createBuffer(event.target.response, false);
+    }
+    xhr.send();
+  };
+  App.noteOn = function (pitch) {
+    var source = App.audioContext.createBufferSource();
+    //var playbackRate = 1;
+    //source.playbackRate.value = playbackRate;
+    source.buffer = App.audioBuffer;
+    source.loop = false;
+    source.connect(App.audioContext.destination);
+    source.noteOn(0);
+    App.audioSources[pitch] = source;
+  };
+  App.noteOff = function (pitch) {
+    App.audioSources[pitch].noteOff(0);
+    App.audioSources[pitch].disconnect();
   };
   App.handleMidi = function (event) {
     var type = event.data[0] >> 4;
@@ -28,6 +57,7 @@
     switch (type) {
       // Note on
       case 9:
+        App.noteOn(pitch);
         if (velocity != 0) {
           var octave = Math.floor(pitch/12) - 1;
           var note = notes[pitch % 12];
@@ -37,6 +67,7 @@
         }
       // Note off
       case 8:
+        App.noteOff(pitch);
         //console.log('Note Off: ', pitch);
         break;
     }
