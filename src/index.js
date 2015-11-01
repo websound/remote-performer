8e13292a734f74ee9bbbd199af814422c22ee205
David Dias <daviddias.p@gmail.com>
2015-11-01 13:38:17 +0000

    migrate to hapi

--- src/index.js
+++ src/index.js
@@ -1,29 +1,51 @@
-var http = require('http')
 var WebSocketServer = require('ws').Server
-var express = require('express')
-var logfmt = require('logfmt')
-var app = express()
-var port = Number(process.env.PORT || 5000)
+var Hapi = require('hapi')
 
-// Static server
-app.use(logfmt.requestLogger())
-app.use(express.static(__dirname + '/public'))
-var server = http.createServer(app)
-server.listen(port)
-console.log('http server listening on %d', port)
+var server = new Hapi.Server({})
 
-// Websocket server
-var wss = new WebSocketServer({server: server})
-console.log('websocket server created')
-wss.on('connection', function (ws) {
-  ws.on('message', function (data, flags) {
-    if (flags.binary) { // If received binary message, i.e. MIDI
-      console.log('MIDI:', data)
-      ws.send(data, {binary: true}) // Echo MIDI message back to client
+server.connection({
+  port: Number(process.env.PORT) || 9090
+})
+
+server.register(require('inert'), function (err) {
+  if (err) {
+    throw err
+  }
+
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: __dirname + '/public'
+      }
     }
   })
-  console.log('websocket connection open')
-  ws.on('close', function () {
-    console.log('websocket connection close')
+
+  server.start(function (err) {
+    if (err) {
+      throw err
+    }
+
+    console.log('Server running at:', server.info.uri)
+    attachWSS()
   })
 })
+
+function attachWSS () {
+  // Websocket server
+  var wss = new WebSocketServer({server: server.listener})
+  console.log('websocket server created')
+  wss.on('connection', function (ws) {
+    ws.on('message', function (data, flags) {
+      if (flags.binary) { // If received binary message, i.e. MIDI
+        console.log('MIDI:', data)
+        ws.send(data, {binary: true}) // Echo MIDI message back to client
+      }
+    })
+    console.log('websocket connection open')
+    ws.on('close', function () {
+      console.log('websocket connection close')
+    })
+  })
+}
